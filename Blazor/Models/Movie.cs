3c295c404fc06fee632823896ce25c451f5efa3a
Tom <r0957117@student.thomasmore.be>
2023-10-09 11:16:43 +0200

    second commit

---
+++ Blazor/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazor.Models
+{
+    public class Movie
+    {
+        public string Poster { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+
+        public Movie()
+        {
+        }
+    }
+}
---
+++ Blazor/Pages/Movies.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using static Blazor.Pages.FetchData;
+using static System.Net.WebRequestMethods;
+
+namespace Blazor.Pages
+{
+    public partial class Movies
+    {
+    }
+}
